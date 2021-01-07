ba95f7f8d074b9e240102192d28d7c1a0c69418f
Alexandre ANDRE <aa.alexandre.andre@gmail.com>
2021-01-07 23:31:48 +0100

    memory can fetch u8 and u16 data

--- src/component/cpu.rs
+++ src/component/cpu.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 
-use crate::component::memory::{ Memory, MemoryError };
+use crate::component::memory::{Memory, MemoryError};
 
-const register_names: &'static [&'static str] = &["ip", "acc", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8"];
+const register_names: &'static [&'static str] =
+    &["ip", "acc", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8"];
 
 /// CPU struct that will be the "head" of the VM.
 /// It handles everything from memory pointers to executing incomming instructions
@@ -14,35 +15,44 @@ pub struct CPU {
 
 impl CPU {
     pub fn new(memory: usize) -> Self {
-        let register_map = register_names.to_vec().iter().fold(HashMap::new(), |mut map, s| {
-            let _ = map.insert(*s, map.len()*2);
-            map
-        });
+        let register_map = register_names
+            .to_vec()
+            .iter()
+            .fold(HashMap::new(), |mut map, s| {
+                let _ = map.insert(*s, map.len() * 2);
+                map
+            });
 
         Self {
             memory: Memory::new(memory),
-            registers: Memory::new(register_names.len()*2),
-            register_map
+            registers: Memory::new(register_names.len() * 2),
+            register_map,
         }
     }
 
-    pub fn get_register(&self, name: &'static str) -> Result<u8, MemoryError> {
-
-        let reg_pointer = self.register_map.get(name).expect("Register name does not exist");
-        self.registers.get_memory_at(*reg_pointer)
+    pub fn get_register(&self, name: &'static str) -> Result<u16, MemoryError> {
+        let reg_pointer = self
+            .register_map
+            .get(name)
+            .expect("Register name does not exist");
+        self.registers.get_memory_at_u16(*reg_pointer)
     }
 
-    pub fn set_register(&mut self, name: &'static str, data: u8) -> Result<(), MemoryError> {
-
-        let reg_pointer = self.register_map.get(name).expect("Register name does not exist");
-        self.registers.set_memory_at(*reg_pointer, data)?;
-
-        Ok(())
+    pub fn set_register(&mut self, name: &'static str, data: u16) -> Result<(), MemoryError> {
+        let reg_pointer = self
+            .register_map
+            .get(name)
+            .expect("Register name does not exist");
+        self.registers.set_memory_at_u16(*reg_pointer, data)
     }
-    
+
     pub fn print_registers(&self) {
         for (reg, pointer) in &self.register_map {
-            println!("register: {}, data: {} {}", *reg, self.registers.get_memory_at(*pointer).unwrap(), self.registers.get_memory_at(*pointer +1).unwrap());
+            println!(
+                "register: {}, data: {:#06X}",
+                *reg,
+                self.registers.get_memory_at_u16(*pointer).unwrap(),
+            );
         }
     }
 }
