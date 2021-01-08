8f04304390f35b71821fa8d40aa09052832fe700
Alexandre ANDRE <aa.alexandre.andre@gmail.com>
2021-01-08 16:29:24 +0100

    update cpu: add fetch_reg() fn. update JNE loop in main code

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -8,21 +8,20 @@ use arch::{
 };
 
 fn main() {
-    let mut cpu = CPU::new(0x100 * 0x100);
+    let mut cpu = CPU::new(0xFF);
     cpu.print_registers();
 
     let instructions = [
-        MOV_MEM_REG,  0x01, 0x00, R1,           // move from 0x0100 address in memory to r1 (16 bit)
-        MOV_LIT_REG,  0x00, 0x01, R2,           // move 0x0001 in r2 (16 bit)
-        ADD_REG_REG,    R1,   R2,               // add r1 and r2
-        MOV_REG_MEM,   ACC, 0x01, 0x00,         // store accumulator value in memory address 0x0100
-        JMP_NOT_EQ,   0x00, 0x03, 0x00, 0x00,   // Jump to address 0x0000 in memory if accumulator not equal to 0x0003
-        END,                                    // stop the program
+        MOV_LIT_REG,  0x00, 0x01, R2,         // move 0x0001 in r2 (16 bit)
+        MOV_REG_REG,  ACC,  R1,               // store accumulator value in memory address 0x0080
+        ADD_REG_REG,  R1,   R2,               // add r1 and r2
+        JMP_NOT_EQ,   0x00, 0x02, 0x00, 0x04, // Jump to address 0x0000 in memory if accumulator not equal to 0x0004
+        END,                                  // stop the program
     ];
 
     cpu.set_instruction(&instructions);
     while cpu.step() {
         cpu.print_registers();
-        cpu.print_memory_chunk_u16(0x0100, 0x0114);
+        // cpu.print_memory_chunk_u16(0x0080, 0x0094);
     }
 }
