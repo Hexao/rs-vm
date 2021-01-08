dcb4152029f8b1b037540e0d576b65b4284344c8
Yohann Jerrain <y.jerrain@gmail.com>
2021-01-08 13:48:26 +0100

    added instructions

--- arch/src/instructions.rs
+++ arch/src/instructions.rs
@@ -1,5 +1,8 @@
 pub const MOV_LIT_REG: u8 = 0x10;
-pub const ADD_REG_REG: u8 = 0x11;
+pub const MOV_REG_REG: u8 = 0x11;
+pub const MOV_REG_MEM: u8 = 0x12;
+pub const MOV_MEM_REG: u8 = 0x13;
+pub const ADD_REG_REG: u8 = 0x16;
 pub const MOV_LIT_R1 : u8 = 0x40;
 pub const MOV_LIT_R2 : u8 = 0x41;
 pub const END        : u8 = 0xFF;
